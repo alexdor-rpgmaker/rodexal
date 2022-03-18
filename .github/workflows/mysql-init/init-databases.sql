f91d72514530729f624e5ae23bf08451b07436bc
Arnaud Doucerain <a.doucerain@gmail.com>
2022-03-18 12:10:50 +0100

    Database tests 2

--- .github/workflows/mysql-init/init-databases.sql
+++ .github/workflows/mysql-init/init-databases.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS `rodexal_test_db`;
 CREATE DATABASE IF NOT EXISTS `rodexal_alexdor_test_db`;
 
-GRANT ALL ON `rodexal_test_db`.* TO 'rodexal_test_db_user'@'%';
-GRANT ALL ON `rodexal_alexdor_test_db`.* TO 'rodexal_test_db_user'@'%';
+GRANT ALL ON `rodexal_test_db`.* TO 'rodexal_test_db_user'@'rodexal_test_db_password';
+GRANT ALL ON `rodexal_alexdor_test_db`.* TO 'rodexal_test_db_user'@'rodexal_test_db_password';
